ff2f7f465f1e74173222c162f4d152b5ce88c8a6
JohnRios809 <j.johnrios@gmail.com>
2025-05-18 22:22:13 -0500

    Delete Proyecto_Sena_NO_BORRAR directory
    Se actualizó la ruta

--- Proyecto_Sena_NO_BORRAR/Base_de_datos/Ecosoft/UsuariosDB.sql
+++
@@ -1,1 +0,0 @@
-SELECT * FROM ecosoft.users;
--- Proyecto_Sena_NO_BORRAR/Base_de_datos/Ecosoft/src/main/java/EcosoftProyecto/Ecosoft/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package EcosoftProyecto.Ecosoft.repository;
-
-
-import EcosoftProyecto.Ecosoft.model.Users;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<Users, Long> {
-}
